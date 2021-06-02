df0d4930eaee4184e274506f7c177ac9483dead6
lsh <rrgks@naver.com>
2021-06-02 15:41:00 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -3,14 +3,32 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const config = require("./config/dev");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://rrgks:dltjrgh123@node-react.ganxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+const { User } = require("./models/User");
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected..."))
 .catch(err => console.log(err));
 
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello Woasdsadrld!"));
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true,
+        })
+    });
+})
 
 app.listen(port, () => console.log(`${port}포트에서 서버 시작`))
