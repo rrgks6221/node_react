e6094040374d702c0aa40a59dc65207790749833
lsh <rrgks@naver.com>
2021-06-07 12:14:54 +0900

    Auth 기능 만들기

--- index.js
+++ index.js
@@ -3,15 +3,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const bodyParser = require("body-parser");
 const config = require("./config/dev");
+const cookieParser = require("cookie-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}))
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 const { User } = require("./models/User");
+const { auth } = require("./middelware/auth");
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected..."))
@@ -20,7 +22,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get("/", (req, res) => res.send("Hello Woasdsadrld!"));
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
 
     user.save((err, userInfo) => {
@@ -28,7 +30,42 @@ app.post('/register', (req, res) => {
         return res.status(200).json({
             success: true,
         })
-    });
+    })
+})
+
+app.post("/api/users/register", (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."});
+
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id })
+            })
+        })
+    })
+})
+
+app.get("/api/users/auth", auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
 })
 
 app.listen(port, () => console.log(`${port}포트에서 서버 시작`))
