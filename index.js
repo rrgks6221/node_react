ad6be0c362b55f81c12344908713af3cf44007bf
lsh <rrgks@naver.com>
2021-06-07 12:39:58 +0900

    로그아웃 기능

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post('/api/users/register', (req, res) => {
     })
 })
 
-app.post("/api/users/register", (req, res) => {
+app.post("/api/users/login", (req, res) => {
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
             return res.json({
@@ -66,6 +66,17 @@ app.get("/api/users/auth", auth, (req, res) => {
         role: req.user.role,
         image: req.user.image
     })
+})
+
+app.get("/api/users/logout", auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id },
+        { token: "" }
+        , (err, user) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            })
+        })
 })
 
 app.listen(port, () => console.log(`${port}포트에서 서버 시작`))
